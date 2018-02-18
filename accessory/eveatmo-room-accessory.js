84eae7f21d898ee6a75f2b1f22355ae8115c0c76
Sebastian Kroll <info@skroll.me>
2018-02-18 16:13:04 +0100

    added "identify" to accessories

--- accessory/eveatmo-room-accessory.js
+++ accessory/eveatmo-room-accessory.js
@@ -66,7 +66,7 @@ module.exports = function(pHomebridge) {
 					this.addService(serviceBattery);
 				}
 
-                this.historyService = new FakeGatoHistoryService("room", this, {size: 4032, storage:'fs', disableTimer: true});
+                this.historyService = new FakeGatoHistoryService("room", this, {storage:'fs', disableTimer: true});
 				
 			} catch (err) {
 				this.log.warn("Could not process service files for " + accessoryConfig.name);
